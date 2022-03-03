6d5d6c9bd152e4bfe4064b25da3297216c196684
g051 <68071451+g051@users.noreply.github.com>
2022-03-03 11:33:39 +0800

    Initial router & pages

---
+++ src/router.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import CoachesList from './pages/coaches/CoachesList';
+import CoachDetail from './pages/coaches/CoachDetail';
+import ContactCoach from './pages/requests/ContactCoach';
+import CoachRegistration from './pages/coaches/CoachRegistration';
+import RequestsReceived from './pages/requests/RequestsReceived';
+import NotFound from './pages/NotFound';
+
+
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id', component: CoachDetail, children: [
+        { path: 'contact', component: ContactCoach }
+      ]
+    },
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: NotFound }
+  ]
+});
+
